4616fc49f37b8774a3d36e472fd6dba9de894bfd
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-08 13:50:09 -0700

    Working on tables

--- data/migrations/20191008132027_bootstrap.js
+++ data/migrations/20191008132027_bootstrap.js
@@ -1,5 +1,16 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("cars", tbl => {});
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments();
+    tbl.string("make");
+    tbl
+      .string("vin")
+      .notNullable()
+      .unique();
+    tbl.string("model").notNullable();
+    tbl.string("mileage").notNullable();
+    tbl.string("transmission");
+    tbl.string("status");
+  });
 };
 
 exports.down = function(knex) {};
