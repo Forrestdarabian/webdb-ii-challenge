9f370d17ee67d622e7a4273969763524c1f34885
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-08 14:31:36 -0700

    CRUD operations done, middleware applied

---
+++ cars/carsRouter.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Cars.get().then(cars => res.status(200).json(cars));
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  Cars.getById(id).then(car => {
+    if (car) {
+      res.status(200).json(car);
+    } else {
+      res.status(404).end();
+    }
+  });
+});
+
+router.post("/", (req, res) => {
+  const { make, model, vin, mileage, transmission, status } = req.body;
+  Cars.insert({ make, model, vin, mileage, transmission, status })
+    .then(car => {
+      if (car) {
+        res.status(200).json(car);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "Error inserting car", message: err.message });
+    });
+});
+module.exports = router;
